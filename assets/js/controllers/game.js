f47a315b9869e5ff060c30b9f0b6145498eddb9d
Ben Baumann <ben@thisisbam.com>
2017-10-07 17:12:43 -0400

    added webpack

---
+++ assets/js/controllers/game.js
@@ -0,0 +1,3 @@
+module.exports = function(){
+    alert("Game on Baby!");
+}
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//     entry: ".assets/js/index.js",
+//     output: {
+//       filename: "bundle.js"
+//     }
+//   }
+
+
+webpack({
+    entry: ".assets/js/index.js",
+    output: {
+      filename: "bundle.js"
+    }
+}, callback);
